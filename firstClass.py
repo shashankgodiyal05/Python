0b4bfd4873d69c149229c69716aa7fef03081057
Shashank Godiyal <42241879+shashankgodiyal05@users.noreply.github.com>
2022-12-13 16:16:46 +0530

    Add files via upload

---
+++ firstClass.py
@@ -0,0 +1,33 @@
+## Date = 08/12/2022
+
+print(5+6)
+print("Output of Addition: ", 5+6)
+print("Python " + "Programing")
+print("Python" + "5", 6)
+print("# is used to write a comment in python")
+
+#this is a comment which will not be executed by the interpreter
+
+var1 = 5861
+var2 = 56892
+
+print("Addition of var1 and var2 is ", var1 + var2)
+print("Substraction of var1 and var2 is ", var1 + var2)
+
+print(var1)
+print(var2)
+
+# for
+# multiple
+# line 
+# coment
+# select
+# the 
+# desired
+# lines 
+# and
+# press 
+# ctrl + /
+
+print("Value of var1 is ", var1)
+print("Value of var2 is ", var2)
---
+++ secondClass.py
@@ -0,0 +1,65 @@
+## Date = 12/12/2022
+
+## Rules to define the variable name:-
+    # Variable name should contain alphabets, numbers, underscore(_)
+    # Variable name's first letter must not be a number
+    # Variable name must be reasonable
+
+    # abc132, _abs, asf, cag_45 are all valid variable names
+    # 123abc, 1_las are all invalid variable names
+
+## input() - this function is used to take input from the user
+# we put required instruction in between (" ")
+# by default it treat input as string
+# int(input()) is used to take integer type of data
+
+var1 = int(input("Enter the first number: "))
+print("You entered ", var1)
+
+var2 = int(input("\nEnter the first number: "))
+print("You entered ", var2)
+
+print("\nAddition of var1 and var2 is ", var1 + var2)
+print("\nSubstraction of var1 and var2 is ", var1 - var2)
+
+## Data Types:-
+
+    ## Primitive Data Types-
+        # 1. int (Integer)
+        # 2. float
+        # 3. complex (2 + 7j type)
+        # 4. bool (Boolean)
+        # 5. str (String)
+        # 6. bytes 
+    # (We can not define a data into bytes but we can convert the other types of data into bytes)
+
+    ## Non Primitive Data Types-
+        # 1. list
+        # 2. tuple
+        # 3. set
+        # 4. frozenset
+        # 5. range
+        # 6. dict (Dictionary)
+        # 7. bytearray
+
+    
+var3 = 58
+print("\nThe type of var3 is", type(var3))
+
+var4 = 51.65
+print("\nThe type of var4 is", type(var4))
+
+var5 = 2 + 7j
+print("\nThe type of var5 is", type(var5))
+
+var6 = True
+print("\nThe type of var6 is", type(var6))
+
+var7 = "Hello frands chai pi lo"
+print("\nThe type of var7 is", type(var7))
+
+var8 = b"python"
+print("\nThe type of var8 is", type(var8))
+
+# to check the type of data stored in the variable we use:-
+# print(type(variable_name))
