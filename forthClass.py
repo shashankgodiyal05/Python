4d3c8564f7526f8c49162e1f25d5b6f1272ab577
Shashank Godiyal <42241879+shashankgodiyal05@users.noreply.github.com>
2022-12-15 18:50:09 +0530

    Add files via upload

---
+++ forthClass.py
@@ -0,0 +1,107 @@
+## Date = 15/12/2022
+
+
+## Type Casting
+
+    # it is an opration which is used to change the data type of the data when required
+
+
+
+#### 1. Converting int into others data types:-
+
+
+var1 = int(input("Enter a number: "))
+
+# converting integer type data into float
+print(var1, "in float:", float(var1))
+
+# converting integer type data into complex
+print(var1, "in complex:", complex(var1))
+
+# converting integer type data into boolean
+print(var1, "in boolean:" ,bool(var1))
+
+# converting integer type data into string
+print(var1,"in string:",str(var1))
+
+# converting integer type data into bytes
+print(var1,"in bytes:",bytes(var1))
+
+
+
+#### 2. Converting float into other data types:-
+
+
+var2 = float(input("Enter the float point number: "))
+
+# converting float into integer
+print(var2, "in integer:", int(var2))
+
+# converting float into complex
+print(var2, "in complex:", complex(var2))
+
+# converting float into bool
+print(var2, "in boolean:", bool(var2))
+
+# converting float into string
+print(var2, "in string", str(var2))
+
+# we can not convert float into bytes
+
+
+
+#### 3. Converting boolean into other data types:-
+
+
+var3 = bool(input("Enter true or false: "))
+
+# converting boolean into int
+print(var3, "in integer:", int(var3))
+
+# converting boolean into float
+print(var3, "in float:", float(var3))
+
+# converting boolean into complex
+print(var3, "in complex:", complex(var3))
+
+# converting boolean into string
+print(var3, "in string:", str(var3))
+
+# converting boolean into bytes
+print(var3, "in bytes:", bytes(var3))
+
+
+
+#### 4. Converting complex no. into other data types:-
+
+
+var4 = complex(input("Enter a complex no.: "))
+
+# we can not convert complex no. into int, float and bytes
+
+# converting complex no. into string
+print(var4, "in string:", str(var4))
+
+# converting complex no. into boolean
+print(var4, "in boolean:", bool(var4))
+
+
+
+########## Number System ###########
+
+    # 1. Decimal number system
+
+        # base 10 (0, 1, 2, 3, 4, 5, 6, 7, 8, 9)
+    
+    # 2. Binary number system
+
+        # base 2 (0, 1)
+        
+    # 3. Hexa decimal number system
+
+        # base 16 (0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 0, A, B, C, D, E, F)
+
+    # 4. Octal number system
+
+        # base 8 (0, 1, 2, 3, 4, 5, 6, 7)
+
